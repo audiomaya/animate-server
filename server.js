7ddff4774cb78a161985daad9f5d7b896f3ed821
audiomaya <audiomaya@hotmail.com>
2015-05-12 23:24:41 -0500

    paso7

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const http = require('http')
 const fs =require('fs')
+const path = require('path')
 const port = process.env.PORT || 8080
 
 const server = http.createServer()
@@ -12,8 +13,14 @@ server.on('listening', onListening)
 server.listen(port)
 
 function onRequest (req, res) {
-  let file = fs.readFileSync('app/index.html')
-  res.end(file)
+  let fileName = path.join(__dirname, 'public', 'index.html')
+
+  fs.readFile(fileName, function(err, file) {
+    if (err) {
+      return res.end(err.message)
+    }
+    res.end(file)
+  })
 }
 
 function onListening () {
